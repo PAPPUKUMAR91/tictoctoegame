a36de67bd8c8adc81f0ede575cf3f1e807b9f5e9
Your Name <your.email@example.com>
2025-10-19 10:27:32 +0530

    all

--- run-console.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting TicTacToe Console Game...
-cd /d E:\JAVAPROJECT
-"C:\Users\pappu\Downloads\OpenJDK17U-jdk_x64_windows_hotspot_17.0.16_8 (1)\jdk-17.0.16+8\bin\javac.exe" TicTacToe.java
-"C:\Users\pappu\Downloads\OpenJDK17U-jdk_x64_windows_hotspot_17.0.16_8 (1)\jdk-17.0.16+8\bin\java.exe" -cp . TicTacToe
-pause
--- run-gui.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting TicTacToe GUI Game...
-cd /d E:\JAVAPROJECT
-"C:\Users\pappu\Downloads\OpenJDK17U-jdk_x64_windows_hotspot_17.0.16_8 (1)\jdk-17.0.16+8\bin\javac.exe" TicTacToeGUI.java
-"C:\Users\pappu\Downloads\OpenJDK17U-jdk_x64_windows_hotspot_17.0.16_8 (1)\jdk-17.0.16+8\bin\java.exe" -cp . TicTacToeGUI
-pause
